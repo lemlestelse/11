b522219c39f03884a9ba75dbda0cc5b626477795
lemlestelse <107282622+lemlestelse@users.noreply.github.com>
2025-05-31 17:33:16 -0300

    Updated bands.ts

--- src/data/bands.ts
+++ src/data/bands.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get, set, onValue } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyApa0ll6QvjZlZt3wodPVCXvjdQ49UxwKA",
+  authDomain: "onlyhatepropaganda-e6e78.firebaseapp.com",
+  databaseURL: "https://onlyhatepropaganda-e6e78-default-rtdb.firebaseio.com",
+  projectId: "onlyhatepropaganda-e6e78",
+  storageBucket: "onlyhatepropaganda-e6e78.firebasestorage.app",
+  messagingSenderId: "128634877550",
+  appId: "1:128634877550:web:37c7aaf41cc5e4edcd987f"
 };
 
 const app = initializeApp(firebaseConfig);
